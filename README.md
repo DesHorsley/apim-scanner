be3e006e3d5836e0ae060e3f512e5df51c2fbc0a
Desmond Horsley <desmond.horsley@health.nsw.gov.au>
2021-10-02 21:54:15 +1000

    Initial commit and config

---
+++ README.md
@@ -0,0 +1,12 @@
+# Azure APIM Scanner
+
+This is an azure function which runs on a timer.
+
+# Deployment
+
+Precondition for the below is that you have a blank azure function already deployed manually.
+
+1. Open this project in vscode
+1. Log in with Azure Function extension
+1. Right click the target function eg "apim-scanner"
+1. Click "Deploy to Function App..."
---
+++ scan/index.js
@@ -0,0 +1,63 @@
+module.exports = async function (context, _myTimer) {
+    const { DefaultAzureCredential } = require("@azure/identity");
+    const { ApiManagementClient } = require("@azure/arm-apimanagement");
+    const subscriptionId = process.env["AZURE_SUBSCRIPTION_ID"];
+    const serviceName = process.env["SERVICE_NAME"];
+    const resourceGroupName = process.env["RESOURCE_GROUP"];
+
+    // Use `DefaultAzureCredential` or any other credential of your choice based on https://aka.ms/azsdk/js/identity/examples
+    // Please note that you can also use credentials from the `@azure/ms-rest-nodeauth` package instead.
+    const creds = new DefaultAzureCredential();
+    const client = new ApiManagementClient(creds, subscriptionId);
+
+    const opts = {}
+    const apisWithNoSubscriptionKey = await client.api.listByService(resourceGroupName, serviceName, opts)
+        .then((result) => {
+            const noSubscriptionKey = result.filter(s => s.subscriptionRequired === false)
+            return Promise.resolve(noSubscriptionKey)
+        })
+
+    const PRODUCTS_WHICH_SECURE_APIS = [
+        'mockAuth'
+    ]
+
+    const insecureProducts = await client.product.listByService(resourceGroupName, serviceName)
+        .then(result => {
+            // remove where subscription is required
+            const noSubscriptions = result.filter(p => p.subscriptionRequired === false);
+
+            // remove where wthe business acknowledges it is a secure product
+            const insecure = noSubscriptions.filter(p => !PRODUCTS_WHICH_SECURE_APIS.includes(p.name))
+            return Promise.resolve(insecure)
+        })
+
+    const insecureProductIds = insecureProducts.map(p => p.id)
+
+    for (const api of apisWithNoSubscriptionKey) {
+        const productsOnApi = await client.apiProduct.listByApis(resourceGroupName, serviceName, api.name)
+        if (!productsOnApi.length) {
+            context.log.error(api.name, 'has no products to secure it')
+        }
+
+        const knownBadProducts = productsOnApi.filter(p => insecureProductIds.includes(p.id))
+
+        if (knownBadProducts.length) {
+            context.log.error(api.name, 'has known bad product', knownBadProducts.map(p => p.name))
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
